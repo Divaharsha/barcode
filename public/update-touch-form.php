ebe15f344ea01619b4cbd40cebcc9359b2072810
jayaprasad356 <jayaprasad356@gmail.com>
2022-10-31 16:16:36 +0530

    update touch completed

--- public/update-touch-form.php
+++ public/update-touch-form.php
@@ -5,38 +5,45 @@ $function = new functions;
 include_once('includes/custom-functions.php');
 $fn = new custom_functions;
 
-
+if (isset($_GET['id'])) {
+    $ID = $db->escapeString($_GET['id']);
+} else {
+    return false;
+    exit(0);
+}
 if (isset($_POST['btnAdd'])) {
-      
-
-        $sql = "SELECT id FROM goldsmith_master ORDER BY id DESC LIMIT 1";
-        $db->sql($sql);
-        $res = $db->getResult();
-        $goldsmith_master_id = $res[0]['id'];
+    
         for ($i = 0; $i < count($_POST['subcategory_id']); $i++) {
             $subcategory_id = $db->escapeString(($_POST['subcategory_id'][$i]));
+            $gmv_id = $db->escapeString(($_POST['gmv_id'][$i]));
             $touch = (isset($_POST['touch'][$i]) && !empty($_POST['touch'][$i])) ? $db->escapeString($fn->xss_clean($_POST['touch'][$i])) : "0";
-            $sql = "INSERT INTO goldsmith_master_variant (goldsmith_master_id,subcategory_id,touch) VALUES('$goldsmith_master_id','" . $subcategory_id . "','$touch')";
+            $sql = "SELECT id FROM goldsmith_master_variant WHERE id = '$gmv_id'";
             $db->sql($sql);
-            $tab_result = $db->getResult();
-        }
-        if (!empty($tab_result)) {
-            $tab_result = 0;
-        } else {
-            $tab_result = 1;
-        }
-        if($tab_result==1){
-            $error['add_menu'] = "<section class='content-header'>
-            <span class='label label-success'>Dealer Goldsmith Master Added Successfully</span>
-            <h4><small><a  href='goldsmithmasters.php'><i class='fa fa-angle-double-left'></i>&nbsp;&nbsp;&nbsp;Back to Goldsmith Master</a></small></h4>
-            </section>";
+            $res = $db->getResult();
+            $num = $db->numRows($res);
+            if ($num >= 1) {
+                $sql = "UPDATE `goldsmith_master_variant` SET `touch`='$touch' WHERE `id` = $gmv_id";
+                $db->sql($sql);
+                $tab_result = $db->getResult();
 
-        }else {
-        $error['add_menu'] = " <span class='label label-danger'>Failed</span>";
 
-    }
+            }else{
+                $sql = "INSERT INTO goldsmith_master_variant (goldsmith_master_id,subcategory_id,touch) VALUES('$ID','$subcategory_id','$touch')";
+                $db->sql($sql);
+                $tab_result = $db->getResult();
+
+
+            }
+
+        }
+        $error['add_menu'] = "<section class='content-header'>
+        <span class='label label-success'>Touch Updated Successfully</span>
+        <h4><small><a  href='goldsmithmasters.php'><i class='fa fa-angle-double-left'></i>&nbsp;&nbsp;&nbsp;Back to Goldsmith Master</a></small></h4>
+        </section>";
 }
-
+$sql_query = "SELECT *,gmv.id AS id FROM `goldsmith_master_variant` gmv,`subcategories` sc WHERE gmv.subcategory_id = sc.id AND gmv.goldsmith_master_id = '$ID' ORDER BY gmv.subcategory_id ";
+$db->sql($sql_query);
+$res = $db->getResult();
 ?>
 <section class="content-header">
     <h1>Update Touch</h1>
@@ -61,22 +68,25 @@ if (isset($_POST['btnAdd'])) {
                 <form id='edit_category_form' method="post" enctype="multipart/form-data">
                     <div class="box-body">
                     <?php
-                            $sql = "SELECT id,name FROM `subcategories`";
+                            $sql = "SELECT id,name FROM `subcategories` ORDER BY id ";
                             $db->sql($sql);
                             $result = $db->getResult();
-                            foreach ($result as $value) {
+                            for ($i = 0; $i < count($result); $i++) {
                             ?>
+                            <input type="hidden" class="form-control" name="subcategory_id[]" value="<?php echo $result[$i]['id']?>" readonly>
+
+                            <input type="hidden" class="form-control" name="gmv_id[]" value="<?php echo (isset($res[$i]['id'])) ? ($res[$i]['id']) : 0;?>">
+
                         <div class="row">
                             <div class="form-group">
                                 <div class='col-md-6'>
                                     <label for="exampleInputEmail1"> Subcategory</label> <i class="text-danger asterik">*</i><?php echo isset($error['name']) ? $error['name'] : ''; ?>
-                                    <input type="text" class="form-control" value="<?php echo $value['name']?>" readonly>
-                                    <input type="hidden" class="form-control" name="subcategory_id[]" value="<?php echo $value['id']?>" readonly>
-
+                                    <input type="text" class="form-control" value="<?php echo $result[$i]['name']?>" readonly>
+                                    
                                 </div>
                                 <div class='col-md-6'>
                                     <label for="exampleInputEmail1"> Touch</label> <i class="text-danger asterik">*</i><?php echo isset($error['name']) ? $error['name'] : ''; ?>
-                                    <input type="text" class="form-control" name="touch[]"  required>
+                                    <input type="text" class="form-control" name="touch[]" value="<?php echo (isset($res[$i]['touch'])) ? ($res[$i]['touch']) : 0;?>"  required>
                                 </div>
                             </div>
                         </div>
